620cf4763985f33d5f5df3f661ceff9972fcff84
Mothi-Dev <mothivnr@gmail.com>
2025-08-24 07:38:15 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+ <form action="Upload.php" method="POST">
+</pre>
+	<center><img src = "bismilla.jpg" width = 80 height = 25></center><pre><table style= "width:100%;"><tr><td><img src = "Logo.png" width = 120 height = 120></td>
+	<td>Island Institute of Tertiary Education<br>
+M.Faaroshige, 6th floor,<br>
+Orchid Magu, 20189.Male,<br>
+Republic of Maldives
+        </td>
+	<td>
+<u><font face = "Verdana" size = "5">STUDENT APPLICATION FORM</u></face>
+</table>
+   FullName:<label style="color:red">*</label>      <input type = "text" name = "sname" size = 50> 
+    ID Number:<label style="color:red">*</label>    <input type = "text"  name = "scellno" size = 15>
+                        
+</form>
+</body>
+</html>
